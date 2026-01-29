b6fc932ed952fb69d2953e13e2fbbdf2635d75b5
Ilham Maslaw <ilhammaslaw1407@gmail.com>
2026-01-29 16:25:46 +0700

    dashboard build

---
+++ client/src/dashboard/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './SideBar'
+
+const MainLayout = () => {
+  return (
+    <div className='min-h-screen bg-slate-100'>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default MainLayout
